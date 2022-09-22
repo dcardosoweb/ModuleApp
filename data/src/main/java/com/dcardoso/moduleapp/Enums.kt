3588df5fd1e9a1adea86570d53782340eac2a1ba
Daniel cardoso <dcardoso.web@gmail.com>
2022-09-22 15:03:28 -0300

    adicionado everwere

--- data/src/main/java/com/dcardoso/moduleapp/Enums.kt
+++ data/src/main/java/com/dcardoso/moduleapp/Enums.kt
@@ -2,6 +2,6 @@ package com.dcardoso.moduleapp
 
 object Enums {
     enum class Direction {
-        NORTH, SOUTH, WEST, EAST, CENTER, NO_DIRECTION
+        NORTH, SOUTH, WEST, EAST, CENTER, NO_DIRECTION, EVERWERE
     }
 }
